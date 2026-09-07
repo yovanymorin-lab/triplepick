9eebc152372dfbb1bf1249f46881ce20db94e2c8
yovanymorin-lab <yovany.morin@gmail.com>
2026-09-07 14:27:04 -0400

    Add files via upload

--- bot.py
+++ bot.py
@@ -12,7 +12,7 @@ import requests
 
 
 # Seguridad: el token NUNCA debe vivir dentro del archivo.
-TOKEN = os.environ.get("8480223191:AAFJkZCS3zDj0tqS8pADtfDu6k9CXRIrWS0", "8480223191:AAFJkZCS3zDj0tqS8pADtfDu6k9CXRIrWS0").strip()
+TOKEN = os.environ.get("TELEGRAM_BOT_TOKEN", "").strip()
 
 # Configuración del envío automático diario.
 AUTO_CHAT_ID = os.environ.get("AUTO_CHAT_ID", "").strip()
